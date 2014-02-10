7bc54b5760deec59c71eca7a2fcdd60dc2083cb7
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-02-10 17:44:00 +0200

    create media migration table

---
+++ app/database/migrations/2014_02_10_154333_create_media_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMediaTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('media', function($table) 
+		{
+			$table -> increments ('id') -> unsigned();
+			$table -> integer ('post_id') -> unsigned();
+			$table -> integer ('media_id') -> unsigned();
+			$table -> string ('media_type');
+			$table -> foreign ('post_id') -> references ('id') -> on ('posts') -> onDelete ('cascade')
+				-> onUpdate ('cascade');
+			$table -> timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('media');
+	}
+
+}
