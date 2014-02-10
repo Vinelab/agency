fefa8818c2efb3fc63415a78fd141801e36111d5
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-02-10 14:46:18 +0200

    add AdminRegistrationEmailNotifier, and AdminRegistrationNotifierInterface from najem

---
+++ app/Agency/Cms/Notifications/AdminRegistrationEmailNotifier.php
@@ -0,0 +1,32 @@
+<?php namespace Agency\Cms\Notifications;
+
+/**
+ * @author Abed Halawi <abed.halawi@vinelab.com>
+ */
+
+use Illuminate\Mail\Mailer;
+use Agency\Cms\Contracts\RegistrableInterface;
+
+class AdminRegistrationEmailNotifier implements Contracts\AdminRegistrationNotifierInterface {
+
+    /**
+     * The mailer instance.
+     *
+     * @var Illuminate\Mail\Mailer
+     */
+    protected $mailer;
+
+    public function __construct(Mailer $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+    public function notify(RegistrableInterface $admin)
+    {
+        $password = $admin->getRegistrationPassword();
+
+        $this->mailer->send('emails.cms.registration', compact('password'), function($message) use($admin) {
+            $message->to($admin->getRegistrationEmail(), $admin->getName())->subject('Welcome!');
+        });
+    }
+}
