887d56d6a8c02433d612cfbbeb9933bb0c2ecf00
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-02-10 16:12:23 +0200

    Add MediaServiceProvider

---
+++ app/Agency/Media/MediaServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Agency\Media;
+
+use Illuminate\Support\ServiceProvider;
+
+use App;
+use Agency\Media\Photos\Manager as PhotosManager;
+use Agency\Media\Photos\AwsPhotoUploader;
+
+class MediaServiceProvider extends ServiceProvider {
+
+    public function register()
+    {
+        $this->app->bind(
+            'Agency\Media\Photos\Contracts\PhotoEditorInterface',
+            'Agency\Media\Photos\InterventionPhotoEditor');
+
+        $this->app->bind(
+            'Agency\Media\Photos\Contracts\PhotoUploaderInterface', function(){
+                return new AwsPhotoUploader(App::make('config'));
+            });
+
+        $this->app->bind(
+            'Agency\Media\Photos\Contracts\ManagerInterface', function(){
+                return new PhotosManager(App::make('config'),
+                                        App::make('Agency\Media\Photos\Contracts\UploadInterface'),
+                                        App::make('Agency\Media\Photos\Photo'),
+                                        App::make('Agency\Media\Photos\Contracts\PhotoEditorInterface'),
+                                        App::make('Agency\Media\Photos\Contracts\PhotoUploaderInterface'));
+            });
+
+        $this->app->bind(
+            'Agency\Media\Photos\Contracts\UploadInterface','Agency\Media\Photos\AwsUpload');
+    }
+}
