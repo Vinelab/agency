7b75aa69903bee08fc3d24ed7c11bc3973ed50c5
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-02-10 18:51:08 +0200

    add extends Repository

--- app/Agency/Cms/Repositories/ContentRepository.php
+++ app/Agency/Cms/Repositories/ContentRepository.php
@@ -4,31 +4,20 @@ use Agency\Cms\Repositories\Contracts\ContentsRepositoryInterface;
 
 use Agency\Cms\Content;
 
-class ContentRepository implements ContentsRepositoryInterface {
+class ContentRepository extends Repository implements ContentsRepositoryInterface {
 
 	protected $content;
 
 
 	public function __construct(Content $content)
 	{
-		$this->content=$content;
-	}
-	
-	public function rules()
-	{
-		return $this->rules;
+		$this->content = $this->model= $content;
 	}
 
 	public function create($title,$alias,$parent_id)
 	{
 		$content = $this->content->create(compact("title","alias","parent_id"));
 		return $content;
-	}
-
-	public function all()
-	{
-		$contents=$this->content->all();
-		return $contents;
 	}
 
 	public function update($id,$title)
@@ -41,27 +30,6 @@ class ContentRepository implements ContentsRepositoryInterface {
 			return $content;
 		}
 			return false;
-	}
-
-	public function delete($id)
-	{
-		$content=$this->content->find($id);
-		if(!is_null($content))
-		{
-			$content->delete();
-			return true;
-		}
-		return false;
-	}
-
-	public function findBy($attribute,$value)
-	{
-		$content=$this->content->where($attribute,$value);
-		if($content->count()!=0)
-		{
-			return $content;
-		}
-		return false;
 	}
 
 	function set($content)
