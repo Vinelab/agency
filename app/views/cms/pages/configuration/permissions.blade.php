f65ac5b9324240b44f245b53fca8280d80a513b9
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-02-11 15:39:09 +0200

    import configuration views from najem

---
+++ app/views/cms/pages/configuration/permissions.blade.php
@@ -0,0 +1,3 @@
+{{-- @author Abed Halawi <abed.halawi@vinelab.com> --}}
+<table id="permissions-table"></table>
+<div id="permissions-pager"></div>
---
+++ app/views/cms/pages/configuration/sections.blade.php
@@ -0,0 +1,3 @@
+{{-- @author Abed Halawi <abed.halawi@vinelab.com> --}}
+<table id="sections-table"></table>
+<div id="sections-pager"></div>
