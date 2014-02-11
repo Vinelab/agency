ed63a1bd33d551656311642e977ac902da194511
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-02-11 21:17:54 +0200

    creat TempsController

---
+++ app/controllers/cms/TempsController.php
@@ -0,0 +1,29 @@
+<?php namespace Agency\Cms\Controllers;
+
+use Agency\Media\Temp\Contracts\TemporaryInterface;
+
+use Input, Response;
+
+class TempsController extends BaseController {
+
+    public function __construct(TemporaryInterface $temp)
+    {
+        $this->temp = $temp;
+    }
+	
+
+    public function storePhotos()
+    {
+        $input = Input::all();
+        $images = $input["images"];
+        $tempImages = [];
+        foreach ($images as $key => $image) {
+           array_push($tempImages, $this->temp->storeImage($image));
+        }
+
+        return json_encode($tempImages);
+
+
+
+    }
+}
