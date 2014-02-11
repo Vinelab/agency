7c7ad6e2cec3d8155d05b793985847fd2aa1e5bf
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-02-11 17:07:31 +0200

    remove commented tabs

--- app/views/cms/layout/templates/sidebar/tabs.blade.php
+++ app/views/cms/layout/templates/sidebar/tabs.blade.php
@@ -25,45 +25,6 @@
 	@endforeach
 </ul>
 
-<!--
-<ul class="nav nav-list">
-	<li class="active">
-		<a href="{{URL::route('cms.dashboard')}}">
-			<i class="icon-dashboard"></i>
-			<span class="menu-text"> Dashboard </span>
-		</a>
-	</li>
-
-	<li>
-		<a href="{{URL::route('cms.artists')}}">
-			<i class="icon-star"></i>
-			<span class="menu-text"> Artists </span>
-		</a>
-	</li>
-
-	<li>
-		<a href="{{URL::route('cms.content')}}">
-			<i class="icon-rss"></i>
-			<span class="menu-text"> Content </span>
-		</a>
-	</li>
-
-	<li>
-		<a href="{{URL::route('cms.audience')}}">
-			<i class="icon-group"></i>
-			<span class="menu-text"> Audience </span>
-		</a>
-	</li>
-
-	<li>
-		<a href="{{URL::route('cms.administration')}}">
-			<i class="icon-list"></i>
-			<span class="menu-text"> Administration </span>
-		</a>
-	</li>
-
-</ul>
--->
 <div class="sidebar-collapse" id="sidebar-collapse">
 	<i class="icon-double-angle-left" data-icon1="icon-double-angle-left" data-icon2="icon-double-angle-right"></i>
 </div>
