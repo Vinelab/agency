01c998ce7205d8f45778d4bf6d64e6837fcbdff9
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-02-19 13:31:04 +0200

    implement mediaInterface

--- app/Agency/Cms/Image.php
+++ app/Agency/Cms/Image.php
@@ -1,13 +1,39 @@
 <?php  namespace Agency\Cms; 
 
-class Image extends \Eloquent  {
+use Agency\Cms\Contracts\MediaInterface;
+
+class Image extends \Eloquent implements MediaInterface  {
 
 	protected $table = "images";
+
 	protected $fillable = ["url"];
 
+	/**
+	 * Get posts
+	 * @return collection of "Agency\Cms\Post"
+	 */
 	public function post()
     {
-        return $this -> morphMany ( "Agency\Cms\Media", "media" );
+        return $this->morphMany ("Agency\Cms\Media", "media");
+    }
+
+    /**
+     * Get Image type 
+     * @return string
+     */
+    public function type()
+    {
+    	return 'image';
+    }
+
+
+    /**
+     * Get Image url
+     * @return string
+     */
+    public function url()
+    {
+    	$this->url;
     }
 
 }
