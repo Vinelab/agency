c59e1d6d48d7204e9ef290da7fde04f1895ec0b1
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-03-04 17:36:48 +0200

    check for privileges to delete and update post

--- app/views/cms/pages/post/index.blade.php
+++ app/views/cms/pages/post/index.blade.php
@@ -21,13 +21,20 @@
                             <img src="{{$post['thumbnail']}}">
                             {{HTML::link(URL::route('cms.post.show',$post['data']->id),$post['data']->title)}}
                             <div class="pull-right action-buttons">
-                                <a class="blue" href="{{URL::route('cms.post.edit',$post['data']->id)}}">
-                                    <i class="icon-pencil bigger-130"></i>
-                                </a>
 
-                                <a class="red" href="{{URL::route('cms.post.destroy',$post['data']->id)}}">
-                                    <i class="icon-trash bigger-130"></i>
-                                </a>
+                                @if ($admin_permissions->has('update'))
+
+                                    <a class="blue" href="{{URL::route('cms.post.edit',$post['data']->id)}}">
+                                        <i class="icon-pencil bigger-130"></i>
+                                    </a>
+                                @endif
+                                
+                                @if ($admin_permissions->has('delete'))
+                                    <a class="red" href="{{URL::route('cms.post.destroy',$post['data']->id)}}">
+                                        <i class="icon-trash bigger-130"></i>
+                                    </a>
+                                @endif
+                                
                             </div>
                         </div>
 
