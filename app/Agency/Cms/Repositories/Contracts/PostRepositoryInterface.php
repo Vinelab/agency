75919d07dca2310c49d85a9381da4a455eb9e91b
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-03-04 20:25:59 +0200

    add publish_date, publish_state to create and update methods

--- app/Agency/Cms/Repositories/Contracts/PostRepositoryInterface.php
+++ app/Agency/Cms/Repositories/Contracts/PostRepositoryInterface.php
@@ -2,11 +2,11 @@
 
 interface PostRepositoryInterface {
 
-	public function create($title,$body,$user_id,$section);
+	public function create($title,$body,$user_id,$section,$publish_date,$publish_state);
 
 	public function getPostsByIds($ids);
 
-	public function update($id,$title,$body,$user_id);
+	public function update($id,$title,$body,$user_id,$section,$publish_date,$publish_state);
 
 	public function publish();
 
