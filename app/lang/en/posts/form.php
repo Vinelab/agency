e51571cf1afaf8c710eb9ce68328d18c2fa54e7f
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-03-06 13:00:44 +0200

    create Lang file

---
+++ app/lang/en/posts/form.php
@@ -0,0 +1,17 @@
+<?php 
+
+return [
+
+    'title'         =>  'Title',
+    'content'       =>  'Content',
+    'add_images'   	=>  'Add Images',
+    'add_videos'    =>  'Add Videos',
+    'add_photo'     =>  'Add Photo',
+    'add_yt_btn'	=>	'Add',
+    'yt_video_url'	=>	'Youtbe video url',
+    'editing'       =>  'Editing',
+    'publish'       =>  'Publish',
+    'schedule'     =>  'Schedule',
+    'published'     =>  'Published',
+    'submit' 		=>	"Submit"
+];
