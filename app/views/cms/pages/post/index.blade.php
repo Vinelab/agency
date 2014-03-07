9bb574dd1e15dd392ef73f8df726d83a2ff31b8d
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-03-07 21:19:09 +0200

    replace id by slug

--- app/views/cms/pages/post/index.blade.php
+++ app/views/cms/pages/post/index.blade.php
@@ -24,13 +24,13 @@
 
                                 @if ($admin_permissions->has('update'))
 
-                                    <a class="blue" href="{{URL::route('cms.post.edit',$post['data']->id)}}">
+                                    <a class="blue" href="{{URL::route('cms.post.edit',$post['data']->slug)}}">
                                         <i class="icon-pencil bigger-130"></i>
                                     </a>
                                 @endif
                                 
                                 @if ($admin_permissions->has('delete'))
-                                    <a class="red" href="{{URL::route('cms.post.destroy',$post['data']->id)}}">
+                                    <a class="red" href="{{URL::route('cms.post.destroy',$post['data']->slug)}}">
                                         <i class="icon-trash bigger-130"></i>
                                     </a>
                                 @endif
