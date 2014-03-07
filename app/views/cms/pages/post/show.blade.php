e6244f7ddc35be7de1c33e115799c5a3ef491873
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-03-07 21:21:44 +0200

    enhance post view

--- app/views/cms/pages/post/show.blade.php
+++ app/views/cms/pages/post/show.blade.php
@@ -1,38 +1,53 @@
 	@section('head')
 		@parent
 		{{HTML::style("/css/post/create.css")}}
+		{{HTML::style("/css/post/show.css")}}
 	@stop
 
 	@section('content')
-		<h2>{{$post->title}}</h2>
+
+	<div id="post-container">
+		<div id="post-title">
+			<h2>{{$post->title}}</h2>
+		</div>
+		<div id="post-cover">
+			@if(!empty($media_array))
+
+				@if($media_array[0]->type()=="image")
+					<img id="cover-photo" src="{{$media_array[0]->url}}">
+				@elseif($media_array[0]->type()=="video")
+					<iframe width="420" height="315" src="{{$media_array[0]->url}}" frameborder="0" allowfullscreen></iframe>
+				@endif
+				
+			@endif
+		</div>
+		<div id="post-body">
+			<p>
+				{{nl2br($post->body)}}
+			</p>
+		</div>
+		<div id="media">
+			@if(sizeof($media_array)>1)
+				@foreach($media_array as $media)
+					@if($media->type()=="image")
+						
+						<img style="width:200px;height200px;" src="{{$media->url}}">
+					@elseif($media->type()=="video")
+						<iframe width="200" height="200" src="{{$media->url}}" frameborder="0" allowfullscreen></iframe>
+					@endif
+				@endforeach
+			@endif
+		</div>
+	</div>
 		
 
 
-		@if(!empty($media_array))
-
-			@if($media_array[0]->type()=="image")
-				<img src="{{$media_array[0]->url}}">
-			@elseif($media_array[0]->type()=="video")
-				<iframe width="420" height="315" src="{{$media_array[0]->url}}" frameborder="0" allowfullscreen></iframe>
-			@endif
-			
-		@endif
 
 
-		<p>
-			{{nl2br($post->body)}}
-		</p>
 
-		@if(sizeof($media_array)>1)
-			@foreach($media_array as $media)
-				@if($media->type()=="image")
-					
-					<img style="width:200px;height200px;" src="{{$media->url}}">
-				@elseif($media->type()=="video")
-					<iframe width="200" height="200" src="{{$media->url}}" frameborder="0" allowfullscreen></iframe>
-				@endif
-			@endforeach
-		@endif
+		
+
+
 		
 	@stop
 
