d2b344a3c724b37f52911a4be7c068dc59280b70
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-03-10 17:38:34 +0200

    this file maps between php Lang class and javascript lang object

---
+++ app/views/cms/pages/post/templates/lang.blade.php
@@ -0,0 +1,15 @@
+<?php
+	// return dd(Lang::get('posts/form.ok'));
+?>
+<script type="text/javascript">
+
+	var lang={};
+	lang.empty_post_msg="{{Lang::get('posts/form.empty_post_msg')}}";
+	lang.ok = "{{Lang::get('posts/form.ok')}}";
+	lang.title_error= "{{Lang::get('posts/form.title_error')}}";
+	lang.invalid_youtube_url="{{Lang::get('posts/form.invalid_youtube_url')}}"
+	lang.conf_messasge_delete_post="{{Lang::get('posts/form.conf_messasge_delete_post')}}"
+	lang.cancel="{{Lang::get('posts/form.cancel')}}"
+	lang.delete="{{Lang::get('posts/form.delete')}}"
+</script>
+
