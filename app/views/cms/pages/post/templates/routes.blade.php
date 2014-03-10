c3d58a73e9454d72263ad718c562860a80344651
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-03-10 17:36:22 +0200

    add additional routes

--- app/views/cms/pages/post/templates/routes.blade.php
+++ app/views/cms/pages/post/templates/routes.blade.php
@@ -1,4 +1,11 @@
 <script type="text/javascript">
 	var routes={};
-	routes.cms_post_remove_photo="{{URL::route('cms.post.remove.photo')}}";
+	routes.cms_post_remove_photo = "{{URL::route('cms.post.remove.photo')}}";
+	routes.cms_post_destroy = "{{URL::route('cms.post.destroy','')}}";
+	routes.cms_delete_temp = "{{URL::route('cms.delete.tmp')}}";
+	routes.cms_post_create = "{{URL::route('cms.post.store')}}";
+	routes.cms_content_show = "{{URL::route('cms.content.show','')}}"
+	routes.cms_post_update = "{{URL::route('cms.post.update','')}}";
+	routes.cms_post_tmp = "{{URL::route('cms.post.tmp')}}";
+	routes.cms_tags = "{{URL::route('cms.tags')}}";
 </script>
