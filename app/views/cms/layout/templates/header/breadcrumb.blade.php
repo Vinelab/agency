499ae04dc7ba024fd9aedacc4b67e524aa5c9d52
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-03-11 13:03:08 +0200

    add id to the list item

--- app/views/cms/layout/templates/header/breadcrumb.blade.php
+++ app/views/cms/layout/templates/header/breadcrumb.blade.php
@@ -1,5 +1,5 @@
 <ul class="breadcrumb">
-	<li class="active">
+	<li id="bread-crumb-first">
 		<i class="icon-{{ $cms_sections['current']->icon }} home-icon"></i>
 		<a href="{{URL::route('cms.' . $cms_sections['current']->alias)}}">
             {{ $cms_sections['current']->title }}
