b9ba662efc95002c7b48867508f2ffa45b37d0d6
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-03-11 19:04:02 +0200

    add publish status and publisher name

--- app/views/cms/pages/content/posts.blade.php
+++ app/views/cms/pages/content/posts.blade.php
@@ -3,15 +3,17 @@
 	{{HTML::style("/css/content/posts.css")}}
 
 @stop
+<?php
+?>
 @section('content')
-        @if ($admin_permissions->has('create'))
-            <div class="row">
-                <a href="{{ URL::route('cms.post.create') }}" class="btn btn-primary">
-                    <span class="icon-plus"></span>
-                    {{Lang::get('posts/form.new_post')}}
-                </a>
-            </div>
-        @endif
+    @if ($admin_permissions->has('create'))
+        <div class="row">
+            <a href="{{ URL::route('cms.post.create') }}" class="btn btn-primary">
+                <span class="icon-plus"></span>
+                {{Lang::get('posts/form.new_post')}}
+            </a>
+        </div>
+    @endif
 	<div class="col-sm-6">
 
         <ol class="dd-list">
@@ -30,6 +32,27 @@
                         </div>
                         <img src="{{$post->thumbnail()}}">
                         {{HTML::link(URL::route('cms.post.show',$post->slug),$post->title)}}
+                        <div class="author-status-container">
+                            <div class="status-container">
+                                @if($post->publish_state == 'published')
+                                    <span class="label label-success arrowed-right arrowed-in pull-right">
+                                        {{Lang::get('posts/form.published')}}: {{$post->publish_date}}
+                                    </span>
+                                @elseif ($post->publish_state == 'editing')
+                                    <span class="label label-info arrowed-right arrowed-in pull-right">
+                                        {{Lang::get('posts/form.editing')}} 
+                                    </span>
+                                @else
+                                    <span class="label label-danger arrowed-right arrowed-in pull-right">
+                                        {{Lang::get('posts/posts.scheduled_at')}} : 
+                                        {{$post->publish_date}}
+                                    </span>
+                                @endif
+                            </div>
+                            <div class="publisher-container">
+                                <h5 class="text-right"><i>{{Lang::get('posts/form.author')}}: {{ Str::title($post->admin()->first()->name)}}</i></h5>
+                            </div>
+                        </div>
                     </div>
                 </li>
 			@endforeach
