9fefee3bd81197fa582486a5e3b71dc2eada4cc6
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-03-12 13:05:27 +0200

    add edit button

--- app/views/cms/pages/post/show.blade.php
+++ app/views/cms/pages/post/show.blade.php
@@ -7,6 +7,12 @@
 	@section('content')
 
 	<div id="post-container">
+		<div id="edit-post-container">
+			<a href="{{URL::route('cms.post.edit',$post->slug)}}" class="btn btn-app btn-primary no-radius">
+				<i class="icon-edit bigger-230"></i>
+				{{Lang::get('posts/form.edit')}}
+			</a>
+		</div>
 		<div id="post-title">
 			<h2>{{$post->title}}</h2>
 		</div>
