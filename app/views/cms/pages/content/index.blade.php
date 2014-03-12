e7b6cb1db297f395c357248fd9e239904da86cba
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-03-12 13:04:31 +0200

    move repetitive codes to a template

--- app/views/cms/pages/content/index.blade.php
+++ app/views/cms/pages/content/index.blade.php
@@ -34,21 +34,7 @@
                             @if(!empty($section_post['posts']))
                                 <ul>
                                     @foreach($section_post['posts'] as $post)
-                                        <li class="posts-container">
-                                            <div class="dd-handle">
-                                                <div class="pull-left action-buttons">
-
-                                                    @if ($admin_permissions->has('update'))
-
-                                                        <a class="blue" href="{{URL::route('cms.post.edit',$post->slug)}}">
-                                                            <i class="icon-pencil bigger-130"></i>
-                                                        </a>
-                                                    @endif
-                                                </div>
-                                                <img src="{{$post->thumbnail()}}">
-                                                    {{HTML::link(URL::route('cms.post.show',$post->slug),$post->title)}}
-                                            </div>
-                                        </li>
+                                        @include('cms.pages.content.templates.post',compact('admin_permissions','post'))
                                     @endforeach
                                 </ul>
                             @endif
--- app/views/cms/pages/content/posts.blade.php
+++ app/views/cms/pages/content/posts.blade.php
@@ -20,41 +20,7 @@
         
 		@if(!empty($posts))
 			@foreach($posts as $post)
-
-				 <li class="posts-container">
-                    <div class="dd-handle">
-                    	<div class="pull-left action-buttons">
-                            @if ($admin_permissions->has('update'))
-                                <a class="blue" href="{{URL::route('cms.post.edit',$post->slug)}}">
-                                    <i class="icon-pencil bigger-130"></i>
-                                </a>
-                            @endif
-                        </div>
-                        <img src="{{$post->thumbnail()}}">
-                        {{HTML::link(URL::route('cms.post.show',$post->slug),$post->title)}}
-                        <div class="author-status-container">
-                            <div class="status-container">
-                                @if($post->publish_state == 'published')
-                                    <span class="label label-success arrowed-right arrowed-in pull-right">
-                                        {{Lang::get('posts/form.published')}}: {{$post->publish_date}}
-                                    </span>
-                                @elseif ($post->publish_state == 'editing')
-                                    <span class="label label-info arrowed-right arrowed-in pull-right">
-                                        {{Lang::get('posts/form.editing')}} 
-                                    </span>
-                                @else
-                                    <span class="label label-danger arrowed-right arrowed-in pull-right">
-                                        {{Lang::get('posts/posts.scheduled_at')}} : 
-                                        {{$post->publish_date}}
-                                    </span>
-                                @endif
-                            </div>
-                            <div class="publisher-container">
-                                <h5 class="text-right"><i>{{Lang::get('posts/form.author')}}: {{ Str::title($post->admin()->first()->name)}}</i></h5>
-                            </div>
-                        </div>
-                    </div>
-                </li>
+				@include('cms.pages.content.templates.post',compact('admin_permissions','post'))
 			@endforeach
 		@endif
 
