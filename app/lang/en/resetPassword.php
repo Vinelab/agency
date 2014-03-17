9f1fe09b9430eb3e954f532244d45faae4d4c7bb
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-03-17 17:00:00 +0200

    update ResetPassword Lang strings

--- app/lang/en/resetPassword.php
+++ app/lang/en/resetPassword.php
@@ -7,6 +7,12 @@ return [
 	'retype_new_password' => 'Retype New Password',
 	'password_updated_successfully' => 'Password Updated Successfully',
 	'password_does_not_match_the_confirm_password' => 'Password does not match the confirm password',
-	'check_ur_mail_msg' => 'An email has been sent to you, please check your inbox!'
+	'check_ur_mail_msg' => 'An email has been sent to you, please check your inbox!',
+	'change_password' => 'Change Password',
+	'old_password' => "Old Password",
+	'save' => "Save",
+	'cancel' => "Cancel",
+	'current_password_error' => "Your current password is not correct",
+	'new_password_cannot_be_empty' => "New password cannot be empty",
 
 ];
