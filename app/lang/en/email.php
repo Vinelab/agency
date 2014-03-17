2d8e070da1bd82ac53f51cd8c87da1f61a4b18e9
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-03-17 13:24:53 +0200

    add email Lang strings

---
+++ app/lang/en/email.php
@@ -0,0 +1,10 @@
+<?php 
+
+return [
+	'reset_password_body' => 'To reset your password go to this link',
+	'click_here' => 'Click Here',
+	'welcome' => 'Welcome',
+	'welcome_to_najem' => 'Welcome to Najem',
+	'use_this_password_to_login' => "use this password to login"
+
+];
