c8a22568a7da381d65df023889c4c84b750800f5
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-03-17 13:26:18 +0200

    resetPassword Lang strings

---
+++ app/lang/en/resetPassword.php
@@ -0,0 +1,12 @@
+<?php 
+
+return [
+	'reset_password' => 'Reset Password',
+	'enter_your_new_password' => 'Enter your new password',
+	'new_password' => 'New Password',
+	'retype_new_password' => 'Retype New Password',
+	'password_updated_successfully' => 'Password Updated Successfully',
+	'password_does_not_match_the_confirm_password' => 'Password does not match the confirm password',
+	'check_ur_mail_msg' => 'An email has been sent to you, please check your inbox!'
+
+];
