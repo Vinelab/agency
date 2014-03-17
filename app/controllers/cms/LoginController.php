da2247e9329544ceff1728569c0f768f5c2765ad
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-03-17 13:14:44 +0200

    implements changePassword and resetPassword methods

--- app/controllers/cms/LoginController.php
+++ app/controllers/cms/LoginController.php
@@ -4,10 +4,14 @@
  * @author Abed Halawi <abed.halawi@vinelab.com>
  */
 
-use View, Input, Redirect, Lang, URL, Auth;
 
 use Agency\Cms\Authentication\Contracts\AuthenticatorInterface;
 use Agency\Cms\Repositories\Contracts\SectionRepositoryInterface;
+use Agency\Cms\Repositories\Contracts\AdminRepositoryInterface;
+use Agency\Cms\Notifications\Contracts\AdminRegistrationNotifierInterface;
+
+use View, Input, Redirect, Lang, URL, Auth, Str, Session;
+
 
 class LoginController extends Controller {
 
@@ -18,11 +22,16 @@ class LoginController extends Controller {
      */
     protected $auth;
 
-    public function __construct(SectionRepositoryInterface $sections, AuthenticatorInterface $auth)
+    public function __construct(SectionRepositoryInterface $sections,
+                                 AuthenticatorInterface $auth,
+                                 AdminRepositoryInterface $admin,
+                                 AdminRegistrationNotifierInterface $notifier)
     {
         parent::__construct($sections);
 
         $this->auth = $auth;
+        $this->admin = $admin;
+        $this->notifier = $notifier;
     }
 
     public function index()
@@ -54,5 +63,39 @@ class LoginController extends Controller {
         $this->auth->logout();
 
         return Redirect::route('cms.login');
+    }
+
+    public function sendMail()
+    {
+        $admin = $this->admin->generateCode(Input::get('email'));
+        $this->notifier->sendCode($admin);
+        Session::flash('warnings',[Lang::get('resetPassword.check_ur_mail_msg')]);
+
+       return Redirect::route('cms.login');
+    }
+
+    public function resetPassword($code)
+    {
+        $user = $this->admin->findBy('code',$code);
+        return View::make('cms.pages.login.resetPassword',compact('user'));
+    }
+
+    public function changePassword()
+    {
+        if(Input::get('password')==Input::get('password-conf'))
+        {
+            try {
+                $admin = $this->admin->findBy('code',Input::get('code'));
+                $this->admin->changePassword($admin,Input::get('password')); 
+                Session::flash('success',[Lang::get('resetPassword.password_updated_successfully')]);
+                return View::make('cms.pages.login.index');
+            } catch (Exception $e) {
+                Session::flash('errors',[$e->getMessage()]);
+            }
+           
+
+        }else{
+            Session::flash('errors',[Lang::get('resetPassword.password_does_not_match_the_confirm_password')]);
+        }
     }
 }
