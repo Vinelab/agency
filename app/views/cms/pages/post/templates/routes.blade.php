b17660980638fd2a5d8f20c67ea4ddf416a50661
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-03-18 09:31:52 +0200

    remove post.remove.photo link

--- app/views/cms/pages/post/templates/routes.blade.php
+++ app/views/cms/pages/post/templates/routes.blade.php
@@ -1,6 +1,5 @@
 <script type="text/javascript">
 	var Routes={};
-	Routes.cms_post_remove_photo = "{{URL::route('cms.post.remove.photo')}}";
 	Routes.cms_post_destroy = "{{URL::route('cms.post.destroy','')}}";
 	Routes.cms_delete_temp = "{{URL::route('cms.delete.tmp')}}";
 	Routes.cms_post_create = "{{URL::route('cms.post.store')}}";
