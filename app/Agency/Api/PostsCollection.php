1d8aecdfa6331abee9ab9e3f83400ddd2dc59b22
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-03-20 17:30:33 +0200

    add total and page property and their getter to postsCollection

--- app/Agency/Api/PostsCollection.php
+++ app/Agency/Api/PostsCollection.php
@@ -2,4 +2,29 @@
 
 use Illuminate\Support\Collection;
 
-class PostsCollection extends Collection {}
+class PostsCollection extends Collection {
+
+	protected $page;
+
+	protected $total;
+
+	public function setPage($page)
+	{
+		$this->page = $page;
+	}
+
+	public function setTotal($total)
+	{
+		$this->total = $total;
+	}
+
+	public function total()
+	{
+		return $this->total;
+	}
+
+	public function page()
+	{
+		return $this->page;
+	}
+}
