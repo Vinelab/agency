8e552652e4c739a703c3b0ac77049647f9350c7e
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-03-21 16:41:08 +0200

    remove submit function

--- app/views/client.blade.php
+++ app/views/client.blade.php
@@ -19,51 +19,6 @@
 		});
 	});
 
-	function submit()
-	{
-		var video = $("#video").prop("files")[0];
-		var title = $("#title").val();
-		var description = $("#description").val();
-		var tags = $("#tags").val();
-
-		var user_name = $("#user_name").val();
-		var email = $("#email").val();
-		var country = $("#country").val();
-		var city = $("#city").val();
-		var age = $("#age").val();
-		var gender = $("#gender").val();
-		var phone = $("#phone").val();
-
-		formdata = new FormData();
-		formdata.append("video",video);
-		formdata.append("title",title);
-		formdata.append("description",description);
-		formdata.append("tags",tags);
-		formdata.append("user_name",user_name);
-		formdata.append("email",email);
-		formdata.append("country",country);
-		formdata.append("city",city);
-		formdata.append("age",age);
-		formdata.append("gender",gender);
-		formdata.append("phone",phone);
-
-		formdata.append("code",code);
-
-		$.ajax({
-		url: "/upload",
-		type: "POST",
-		data: formdata,
-		processData: false,
-		contentType: false,
-		success: function (res) {
-			console.log(res);
-		}
-	});
-
-
-
-		console.log(video);
-	}
 
 
 
