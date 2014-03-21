16b33fa3ce67348d9dc87a6e02495a43af6ff1e9
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-03-21 15:18:49 +0200

    bind EncryptorValidator to its interface, fix encryptor namespace

--- app/Agency/Api/ApiServiceProvider.php
+++ app/Agency/Api/ApiServiceProvider.php
@@ -8,11 +8,12 @@ class ApiServiceProvider extends ServiceProvider {
     {
         $this->app->bind('Agency\Api\Repositories\Contracts\ApplicationRepositoryInterface','Agency\Api\Repositories\ApplicationRepository');
         $this->app->bind('Agency\Api\Repositories\Contracts\CodeRepositoryInterface','Agency\Api\Repositories\CodeRepository');
-        $this->app->bind('Agency\Api\EncryptorInterface','Agency\Api\Encryptor');
+        $this->app->bind('Agency\Api\Encryptors\EncryptorInterface','Agency\Api\Encryptors\Encryptor');
         
         $this->app->bind('Agency\Api\Validators\Contracts\ApplicationValidatorInterface','Agency\Api\Validators\ApplicationValidator');
         $this->app->bind('Agency\Api\Validators\Contracts\CodeValidatorInterface','Agency\Api\Validators\CodeValidator');
         $this->app->bind('Agency\Api\Validators\Contracts\SystemValidatorInterface','Agency\Api\Validators\SystemValidator');
+        $this->app->bind('Agency\Api\Validators\Contracts\EncryptorValidatorInterface','Agency\Api\Validators\EncryptorValidator');
 
 
     }
