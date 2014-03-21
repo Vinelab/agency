34cfa1e1d59fe15f78636ec6675991dadb04729a
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-03-21 15:23:36 +0200

    check if code is valid

--- app/controllers/Api/TagsController.php
+++ app/controllers/Api/TagsController.php
@@ -1,6 +1,7 @@
 <?php namespace Agency\Api\Controllers;
 
 use Agency\Cms\Repositories\Contracts\TagRepositoryInterface;
+use Agency\Api\Repositories\Contracts\CodeRepositoryInterface;
 
 use Input, Response, File, DB, Lang;
 
@@ -10,20 +11,26 @@ use Agency\Api\Mappers\TagMapper;
 
 class TagsController extends \Controller {
 
-    public function __construct( TagRepositoryInterface $tag)
+    public function __construct(    TagRepositoryInterface $tag,
+                                    CodeRepositoryInterface $code)
     {
         $this->tag = $tag;
         $this->tagMapper = new TagMapper();
+        $this->code = $code;
 
     }
 
     public function index()
     {
-        $tags = $this->tag->all();
-        if(!$tags->isEmpty())
+        if($this->code->findBy("code",Input::get('code')))
         {
-            return dd($this->tagMapper->make($tags)->toArray());
+            $tags = $this->tag->all();
+            if(!$tags->isEmpty())
+            {
+                return $this->tagMapper->make($tags)->toArray();
+            }
+        } else {
+            return Response::json(['status'=>400,'messages'=>Lang::get("messages.invalid_code")]);
         }
-
     }
 }
