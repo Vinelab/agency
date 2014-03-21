4ead034ccbb9daacde813c0f9a3db7a17db1783e
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-03-21 15:23:08 +0200

    check if code is valid

--- app/controllers/Api/PostsController.php
+++ app/controllers/Api/PostsController.php
@@ -3,6 +3,7 @@
 use Agency\Cms\Repositories\Contracts\PostRepositoryInterface;
 use Agency\Cms\Repositories\Contracts\SectionRepositoryInterface;
 use Agency\Cms\Repositories\Contracts\TagRepositoryInterface;
+use Agency\Api\Repositories\Contracts\CodeRepositoryInterface;
 
 use Input, Response, File, DB, Lang;
 
@@ -18,31 +19,49 @@ class PostsController extends \Controller {
 
     public function __construct( PostRepositoryInterface $post,
     							 SectionRepositoryInterface $section,
-    							 TagRepositoryInterface $tag)
+    							 TagRepositoryInterface $tag,
+                                 CodeRepositoryInterface $code)
     {
         $this->post = $post;
         $this->section = $section;
         $this->tag = $tag;
         $this->postMapper = new PostMapper();
         $this->postsCollection = new PostsCollection();
+        $this->code = $code;
     }
 
     public function index()
     {
-        $posts = $this->post->allPublished(Input::all());
-        return Api::respond($posts->toArray(),$posts->total(), $posts->page());
+        if($this->code->findBy("code",Input::get('code')))
+        {
+            $posts = $this->post->allPublished(Input::all());
+            if(get_class($posts)=='Agency\Api\PostsCollection')
+            {
+                return Api::respond($posts->toArray(),$posts->total(), $posts->page());
+            } else {
+                return $posts;
+            }
+        } else {
+            return Response::json(['status'=>400,'messages'=>Lang::get("messages.invalid_code")]);
+        }
+        
     }
 
 
     public function show($idOrSlug)
     {
-        $post = $this->post->findByIdOrSlug($idOrSlug);
-        if(!is_null($post))
-        {
-            $post = $this->postMapper->make($post);
-            return Api::respond($post,1,1);
+        if($this->code->findBy("code",Input::get('code')))
+        {   
+            $post = $this->post->findByIdOrSlug($idOrSlug);
+            if(!is_null($post))
+            {
+                $post = $this->postMapper->make($post);
+                return Api::respond($post,1,1);
+            } else {
+                return Response::json(['status'=>'400','message'=>Lang::get('api/posts.not_found')]);
+            }
         } else {
-            return Response::json(['status'=>'400','message'=>Lang::get('api/posts.not_found')]);
+            return Response::json(['status'=>400,'messages'=>Lang::get("messages.invalid_code")]);
         }
     }
 }
