49aaf492adad357da20303e0715044314cd55aa0
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-03-24 11:48:33 +0200

    add arabic support

---
+++ app/lang/ar/email.php
@@ -0,0 +1,10 @@
+<?php 
+
+return [
+	'reset_password_body' => 'لإعادة تعيين كلمة السرالرجاء الذهاب الى هذا الرابط',
+	'click_here' => 'إكبس هنا',
+	'welcome' => 'مرحبا',
+	'welcome_to_najem' => 'أهلا وسهلاا بك',
+	'use_this_password_to_login' => "إستعمل كلمة السر هذه للدخول"
+
+];
