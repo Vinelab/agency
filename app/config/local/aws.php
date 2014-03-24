56ad9d3445dce6dcb9d0cfd09ac10f20c7596641
Abed Halawi <halawi.abed@gmail.com>
2014-03-24 17:01:55 +0200

    remove aws credentials

--- app/config/local/aws.php
+++ app/config/local/aws.php
@@ -5,7 +5,7 @@ return [
 
         'bucket' => 'awsfacebookapp',
 
-        'credentials' => ['key'    => 'AKIAIEH3GR3LYFIZEFLQ', 'secret' => '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']
+        'credentials' => ['key'    => '', 'secret' => '']
 
     ]
 ];
