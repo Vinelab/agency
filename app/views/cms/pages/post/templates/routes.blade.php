3c5941be7a34f3697f222e17d6ab3d37618ca0e8
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-03-25 19:08:04 +0200

    remove unneed route

--- app/views/cms/pages/post/templates/routes.blade.php
+++ app/views/cms/pages/post/templates/routes.blade.php
@@ -1,7 +1,6 @@
 <script type="text/javascript">
 	var Routes={};
 	Routes.cms_post_destroy = "{{URL::route('cms.content.posts.destroy','')}}";
-	Routes.cms_delete_temp = "{{URL::route('cms.content.posts.photos.delete')}}";
 	Routes.cms_post_create = "{{URL::route('cms.content.posts.store')}}";
 	Routes.cms_content_show = "{{URL::route('cms.content.show','')}}"
 	Routes.cms_post_update = "{{URL::route('cms.content.posts.update','')}}";
