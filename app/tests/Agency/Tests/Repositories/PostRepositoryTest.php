e76f3550f7cf4bd63e63da04f2d10d2000385ec7
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-03-27 21:36:15 +0200

    mock image interface in PostRepositoryTest

--- app/tests/Agency/Tests/Repositories/PostRepositoryTest.php
+++ app/tests/Agency/Tests/Repositories/PostRepositoryTest.php
@@ -19,10 +19,11 @@ class PostRepositoryTest extends TestCase {
         parent::setUp();
 
         $this->mPost = M::mock('Agency\Post');
+        $this->mImage = M::mock('Agency\Contracts\ImageInterface');
         $this->images = M::mock('Agency\Repositories\Contracts\ImageRepositoryInterface');
         $this->sections = M::mock('Agency\Repositories\Contracts\SectionRepositoryInterface');
 
-        $this->posts = new PostRepository($this->mPost, $this->images, $this->sections);
+        $this->posts = new PostRepository($this->mPost, $this->images, $this->sections, $this->mImage);
     }
 
     public function test_posts_provider_binding()
