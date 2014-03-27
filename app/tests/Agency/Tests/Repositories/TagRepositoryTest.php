4afde8b89274387d248c4a1ae78be219e18a0a56
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-03-27 21:36:44 +0200

    test create multiple tags

--- app/tests/Agency/Tests/Repositories/TagRepositoryTest.php
+++ app/tests/Agency/Tests/Repositories/TagRepositoryTest.php
@@ -5,6 +5,7 @@
  */
 
 use TestCase, Mockery as M;
+use Agency\Tag;
 use Agency\Repositories\TagRepository;
 
 class TagRepositoryTest extends TestCase {
@@ -32,10 +33,29 @@ class TagRepositoryTest extends TestCase {
     {
         $text = $slug = 'my-tag';
 
+        $this->mTag->shouldReceive('whereRaw')->once()->andReturn($this->mTag);
+        $this->mTag->shouldReceive('count')->once()->andReturn(0);
         $this->mTag->shouldReceive('firstOrCreate')->once()
             ->with(compact('text', 'slug'))->andReturn($this->mTag);
         $tag = $this->tags->create($text);
 
         $this->assertInstanceOf('Agency\Tag', $tag);
+    }
+
+    public function test_creating_multiple_tags()
+    {
+        $tags = ['my tag', 'another tag', 'some tag here'];
+        $coll = M::mock('Illuminate\Database\Eloquent\Collection');
+
+        $this->mTag
+            ->shouldReceive('whereRaw')->times(count($tags))->andReturn($this->mTag)
+            ->shouldReceive('count')->times(count($tags))->andReturn(0)
+            ->shouldReceive('saveMany')->with(M::type('array'))
+                ->times(count($tags))
+                ->andReturn($this->mTag)
+            ->shouldReceive('list')->once()->with('id')->andReturn($coll);
+
+        $saved = $this->tags->createMany($tags);
+        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $saved);
     }
 }
