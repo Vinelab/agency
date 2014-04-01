7b36bbff91162a92b5adbf3c341e1c743507da1f
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-04-01 12:41:30 +0300

    add routes

--- app/views/cms/pages/post/templates/routes.blade.php
+++ app/views/cms/pages/post/templates/routes.blade.php
@@ -4,6 +4,7 @@
 	Routes.cms_post_create = "{{URL::route('cms.content.posts.store')}}";
 	Routes.cms_content_show = "{{URL::route('cms.content.show','')}}"
 	Routes.cms_post_update = "{{URL::route('cms.content.posts.update','')}}";
-	Routes.cms_post_tmp = "{{URL::route('cms.content.posts.photos')}}";
+	Routes.cms_post_tmp = "{{URL::route('cms.content.posts.photos.store')}}";
+	Routes.cms_post_photos_destroy = "{{URL::route('cms.content.posts.photos.destroy','')}}"
 	Routes.cms_tags = "{{URL::route('cms.content.posts.tags')}}";
 </script>
