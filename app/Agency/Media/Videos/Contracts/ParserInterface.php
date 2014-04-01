9bb58fdf5d4177c1bfca895db4de104a02e1a2ef
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-04-01 17:15:55 +0300

    remove ValidateYoutubeUrl from the parser interface and integrate this functionality with VideoValidator through custom validation

--- app/Agency/Media/Videos/Contracts/ParserInterface.php
+++ app/Agency/Media/Videos/Contracts/ParserInterface.php
@@ -4,13 +4,4 @@ interface ParserInterface {
 
 	public function make($videos);
 
-	/**
-	 * validate the passed URL for being
-	 * a youtube URL or not
-	 *
-	 * @param {string} $url
-	 * @return boolean
-	 */
-	public function validateYoutubeUrl($url);
-
 }
