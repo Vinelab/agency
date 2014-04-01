6242e1211b25dcf2332d4d8811a6781b59564cab
Ibrahim Fleifel <bob.fleifel@gmail.com>
2014-04-01 20:20:46 +0300

    replace slugify with aliasify method

--- app/Agency/Section.php
+++ app/Agency/Section.php
@@ -21,9 +21,10 @@ class Section extends Eloquent implements PrivilegableInterface {
 
         static::saving(function($model){
 
+            $helper = new Helper();
             if ( ! isset($model->alias) or empty($model->alias))
             {
-                $model->alias = Helper::slugify($model->title,static::$this);
+                $model->alias = $helper->aliasify($model->title);
             }
 
         });
