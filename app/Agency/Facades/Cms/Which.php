674e1180787381bfa6b439775a6c8ce7eb1a8caa
ibrahim fleifle <ibrahimfleifle@flay.local>
2014-11-18 16:32:42 +0100

    create Which Facade

---
+++ app/Agency/Facades/Cms/Which.php
@@ -0,0 +1,12 @@
+<?php namespace Agency\Facades\Cms;
+
+/**
+ * @author Abed Halawi <abed.halawi@vinelab.com>
+ */
+
+use Illuminate\Support\Facades\Facade;
+
+class Which extends Facade {
+
+    public static function getFacadeAccessor() { return 'which'; }
+}
