6b650c4c9fe6f53363e34efd68e99d451ce9e8b7
ibrahim fleifle <ibrahimfleifle@flay.local>
2014-11-19 00:17:24 +0100

    create Dashboard Controller

---
+++ app/controllers/Office/DashboardController.php
@@ -0,0 +1,15 @@
+<?php namespace Agency\Office\Controllers;
+
+/**
+ * @author Abed Halawi <abed.halawi@vinelab.com>
+ */
+
+use View, Auth;
+
+class DashboardController extends Controller {
+
+    public function index()
+    {
+        return View::make('cms.pages.dashboard.index', compact('errors', 'warnings', 'success'));
+    }
+}
