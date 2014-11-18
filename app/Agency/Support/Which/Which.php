63e27a827cdb7a1777f04b22f50e059b51310a08
ibrahim fleifle <ibrahimfleifle@flay.local>
2014-11-18 16:24:48 +0100

    create Which classe

---
+++ app/Agency/Support/Which/Which.php
@@ -0,0 +1,40 @@
+<?php namespace Agency\Support\Which;
+
+/**
+ * @author Abed Halawi <abed.halawi@vinelab.com>
+ */
+
+class Which {
+
+    /**
+     * @var \Agency\Support\Which\Sections
+     */
+    private $sections;
+
+    public function __construct(Sections $sections, Artists $artists)
+    {
+        $this->artists = $artists;
+        $this->sections = $sections;
+    }
+
+    /**
+     * Get the current section being visited.
+     *
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function section()
+    {
+        return $this->sections->current();
+    }
+
+    public function category()
+    {
+        return $this->sections->currentCategory();
+    }
+
+    public function artist()
+    {
+        return $this->artists->current();
+    }
+
+}
