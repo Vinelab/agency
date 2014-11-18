30066e00da06e12aedc9253f69ea158eaff6b838
ibrahim fleifle <ibrahimfleifle@flay.local>
2014-11-18 23:57:34 +0100

    replace sms_sections with the value of current_section

--- app/views/cms/layout/templates/header/breadcrumb.blade.php
+++ app/views/cms/layout/templates/header/breadcrumb.blade.php
@@ -1,22 +1,9 @@
 <ul class="breadcrumb">
-	<li id="bread-crumb-first">
-		<i class="icon-{{ $cms_sections['current']->icon }} home-icon"></i>
-		<a href="{{URL::route('cms.' . $cms_sections['current']->alias)}}">
-            {{ $cms_sections['current']->title }}
+	<li class="active">
+    <?php $current_section = Which::section(); ?>
+		<i class="ace-icon  fa fa-{{ $current_section->icon }} home-icon"></i>
+		<a href="{{URL::route('cms.' . $current_section->alias)}}">
+            {{ $current_section->title }}
         </a>
 	</li>
-
-	@if(isset($parent_sections))
-		@foreach($parent_sections as $section)
-			<li>
-	        	<a href="{{URL::route('cms.content.show',$section->alias)}}">
-	            	{{$section->title}}
-	        	</a>
-        	</li>
-		@endforeach	
-	@endif
-
-	
-	
-	
 </ul><!-- .breadcrumb -->
