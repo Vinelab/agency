40022f4197f8bdeeaf30c626007fc5f02546d5d9
ibrahim fleifle <ibrahimfleifle@flay.local>
2014-11-18 13:31:06 +0100

    create Section Model

---
+++ app/Agency/Office/Section.php
@@ -0,0 +1,52 @@
+<?php namespace Agency\Office;
+
+/**
+ * @author Abed Halawi <abed.halawi@vinelab.com>
+ */
+
+use NeoEloquent;
+use Agency\Helper;
+
+use Agency\Contracts\Office\PrivilegableInterface;
+
+class Section extends NeoEloquent implements PrivilegableInterface {
+
+    protected $label = ['Section', 'Agency', 'Cms'];
+
+    protected $fillable = ['title', 'alias', 'icon', 'is_fertile', 'is_roleable'];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::saving(function($model){
+
+            if ( ! isset($model->alias) || empty($model->alias))
+            {
+                $model->alias = Helper::aliasify($model->title);
+            }
+
+        });
+    }
+
+    public function privileges()
+    {
+        return $this->morphMany('Agency\Office\Auth\Authorization\Entities\Privilege', 'resource');
+    }
+
+    public function children()
+    {
+        return $this->hasMany('Agency\Office\Section', 'PARENT_OF');
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo('Agency\Office\Section', 'PARENT_OF');
+    }
+
+    public function alias()
+    {
+        return $this->alias;
+    }
+
+}
