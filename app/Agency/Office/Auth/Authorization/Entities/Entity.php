57574c6f9400795e6e21feb82e11404ac87f66d5
ibrahim fleifle <ibrahimfleifle@flay.local>
2014-11-18 13:31:59 +0100

    create Entity Model

---
+++ app/Agency/Office/Auth/Authorization/Entities/Entity.php
@@ -0,0 +1,26 @@
+<?php namespace Agency\Office\Auth\Authorization\Entities;
+
+use NeoEloquent;
+use Agency\Helper;
+
+class Entity extends NeoEloquent {
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::saving(function($model){
+
+            if ( ! isset($model->alias) || empty($model->alias))
+            {
+                $model->alias = Helper::aliasify($model->title);
+            }
+
+        });
+    }
+
+    public function dbTable()
+    {
+        return $this->table;
+    }
+}
