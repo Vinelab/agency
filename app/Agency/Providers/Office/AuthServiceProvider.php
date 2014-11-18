f7f554fd33af0766d496f1260d4d625059ddd0ed
ibrahim fleifle <ibrahimfleifle@flay.local>
2014-11-18 16:02:58 +0100

    create AuthServiceProvider

---
+++ app/Agency/Providers/Office/AuthServiceProvider.php
@@ -0,0 +1,26 @@
+<?php namespace Agency\Providers\Office;
+
+/**
+ * @author Abed Halawi <abed.halawi@vinelab.com>
+ */
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\AliasLoader;
+
+class AuthServiceProvider extends ServiceProvider {
+
+    public function register()
+    {
+        $this->app->bind(
+            'Agency\Contracts\Office\Repositories\PermissionRepositoryInterface',
+            'Agency\Office\Auth\Repositories\PermissionRepository');
+
+        $this->app->bind(
+            'Agency\Contracts\Office\Repositories\PrivilegeRepositoryInterface',
+            'Agency\Office\Auth\Repositories\PrivilegeRepository');
+
+        $this->app->bind(
+            'Agency\Contracts\Office\Repositories\RoleRepositoryInterface',
+            'Agency\Office\Auth\Repositories\RoleRepository');
+    }
+}
