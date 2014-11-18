7ba1330565302da5142a3bb9014b8c13ec7a8242
ibrahim fleifle <ibrahimfleifle@flay.local>
2014-11-18 14:16:27 +0100

    create Role Repository

---
+++ app/Agency/Office/Repositories/RoleRepository.php
@@ -0,0 +1,9 @@
+<?php namespace Agency\Office\Repositories;
+
+use Agency\Office\Auth\Authorization\Entities\Role;
+use Agency\Contracts\Office\Repositories\RoleRepositoryInterface;
+
+class RoleRepository extends Repository implements RoleRepositoryInterface {
+
+
+}
