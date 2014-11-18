565fbe052da96b20f2c386b494456173ece9efb3
ibrahim fleifle <ibrahimfleifle@flay.local>
2014-11-18 13:46:46 +0100

    create PermissionsCollection

---
+++ app/Agency/Office/Auth/Authorization/PermissionsCollection.php
@@ -0,0 +1,9 @@
+<?php namespace Agency\Office\Auth\Authorization;
+
+/**
+ * @author Abed Halawi <abed.halawi@vinelab.com>
+ */
+
+use Illuminate\Support\Collection;
+
+class PermissionsCollection extends Collection {}
