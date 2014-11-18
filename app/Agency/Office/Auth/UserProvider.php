8c4b3108787c0b40a16d7c2415bf1e9c6dc58b4c
ibrahim fleifle <ibrahimfleifle@flay.local>
2014-11-18 13:52:59 +0100

    create userProvider

---
+++ app/Agency/Office/Auth/UserProvider.php
@@ -0,0 +1,9 @@
+<?php namespace Agency\Office\Auth;
+
+/**
+ * @author Abed Halawi <abed.halawi@vinelab.com>
+ */
+
+use Illuminate\Auth\EloquentUserProvider;
+
+class UserProvider extends EloquentUserProvider { }
