0a62c975edf8267a4084057c1c4df4cc0feaaf25
ibrahim fleifle <ibrahimfleifle@flay.local>
2014-11-18 13:37:09 +0100

    create AuthorableInterface

---
+++ app/Agency/Contracts/Office/AuthorableInterface.php
@@ -0,0 +1,15 @@
+<?php namespace Agency\Contracts\Office;
+
+/**
+ * @author Abed Halawi <abed.halawi@vinelab.com>
+ */
+
+interface AuthorableInterface {
+
+    /**
+     * The record identifier
+     * @return int | string
+     */
+    public function getKey();
+
+}
