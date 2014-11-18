8c314ed7cf97cf28c82a5544fd76d853d9198daa
ibrahim fleifle <ibrahimfleifle@flay.local>
2014-11-18 13:34:47 +0100

    create RegistrableInterface

---
+++ app/Agency/Contracts/Office/RegistrableInterface.php
@@ -0,0 +1,32 @@
+<?php namespace Agency\Contracts\Office;
+
+/**
+ * @author Abed Halawi <abed.halawi@vinelab.com>
+ */
+
+interface RegistrableInterface {
+
+    /**
+     * Returns the password that was generated
+     * after registration.
+     *
+     * @return string
+     */
+    public function getRegistrationPassword();
+
+    /**
+     * Returns the email to which we
+     * should be sending the registration
+     * email.
+     *
+     * @return string
+     */
+    public function getRegistrationEmail();
+
+    /**
+     * Returns the name of the registered user.
+     *
+     * @return string
+     */
+    public function getName();
+}
