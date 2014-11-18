fa0a8af5d4d973aa6d53a5a631df7a446a72ba6b
ibrahim fleifle <ibrahimfleifle@flay.local>
2014-11-18 14:16:46 +0100

    create Base Validator class

---
+++ app/Agency/Office/Validators/Validator.php
@@ -0,0 +1,42 @@
+<?php namespace Agency\Office\Validators;
+
+/**
+ * @author Abed Halawi <abed.halawi@vinelab.com>
+ */
+
+use Illuminate\Validation\Factory as ValidatorFactory;
+use Agency\Contracts\Office\Validators\ValidatorInterface;
+
+abstract class Validator implements ValidatorInterface {
+
+    /**
+     * The validator instance.
+     *
+     * @var Illuminate\Validation\Factory
+     */
+    protected $validator;
+
+    /**
+     * The rules to validate against.
+     *
+     * @var array
+     */
+    protected $rules;
+
+    /**
+     * Create a new validator instance.
+     *
+     * @param Illuminate\Validator\Factory $validator
+     */
+    public function __construct(ValidatorFactory $validator)
+    {
+        $this->validator = $validator;
+    }
+
+    public function validation($attributes)
+    {
+        return $this->validator->make($attributes, $this->rules);
+    }
+
+    abstract function validate($attributes);
+}
