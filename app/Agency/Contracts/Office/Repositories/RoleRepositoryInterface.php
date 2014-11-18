d9b112c5fa7d4495241672a24438a2a6134410c6
ibrahim fleifle <ibrahimfleifle@flay.local>
2014-11-18 14:20:32 +0100

    create RoleRepositoryInterface

---
+++ app/Agency/Contracts/Office/Repositories/RoleRepositoryInterface.php
@@ -0,0 +1,21 @@
+<?php namespace Agency\Contracts\Office\Repositories;
+
+interface RoleRepositoryInterface {
+
+	/**
+	 * Return the artist's roles.
+	 * @param array $ids
+	 * @return mixed
+	 */
+	public function forArtists($ids = []);
+
+    public function create($title, $alias, $for_artists = false);
+
+    public function update($id, $title, $alias, $for_artists = false);
+
+    public function updatePermissions($id, $permission_ids);
+
+    public function allWithPermissions();
+
+    public function allWithArtistsPermissions();
+}
