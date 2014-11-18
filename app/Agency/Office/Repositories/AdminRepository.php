1c67e997160d8688b35711254df73fb5f51732c5
ibrahim fleifle <ibrahimfleifle@flay.local>
2014-11-18 14:16:20 +0100

    create Admin Repository

---
+++ app/Agency/Office/Repositories/AdminRepository.php
@@ -0,0 +1,72 @@
+<?php namespace Agency\Office\Repositories;
+
+/**
+ * @author Abed Halawi <abed.halawi@vinelab.com>
+ */
+
+use Hash;
+use Agency\Office\Admin;
+use Agency\Contracts\Office\Repositories\AdminRepositoryInterface;
+
+class AdminRepository extends Repository implements AdminRepositoryInterface {
+
+    /**
+     * The admin model instance.
+     *
+     * @var Agency\Office\Admin
+     */
+    protected $admin;
+
+    public function __construct(Admin $admin)
+    {
+        $this->model = $this->admin = $admin;
+    }
+
+    /**
+     * Create a new Admin.
+     *
+     * @param  string $name
+     * @param  string $email
+     * @return Agency\Office\Admin
+     */
+    public function create($name, $email)
+    {
+        $raw = $this->generatePassword();
+
+        $password  = Hash::make($raw);
+
+        $admin = $this->admin->create(compact('name', 'email', 'password'));
+
+        $admin->raw_password = $raw;
+
+        return $admin;
+    }
+
+    /**
+     * Update an existing Admin.
+     *
+     * @param  mixed $id
+     * @param  string $name
+     * @param  string $email
+     * @return Agency\Office\Admin
+     */
+    public function update($id, $name, $email)
+    {
+        $admin = $this->find($id);
+        $admin->fill(compact('name', 'email'));
+
+        $admin->save();
+
+        return $admin;
+    }
+
+    /**
+     * Generates a random password.
+     *
+     * @return string
+     */
+    protected function generatePassword()
+    {
+        return uniqid();
+    }
+}
