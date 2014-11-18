2e20fc62aec051912eaefe9bb965f0700536a6c6
ibrahim fleifle <ibrahimfleifle@flay.local>
2014-11-18 14:02:32 +0100

    create the base exception class

---
+++ app/Agency/Office/Auth/Authorization/Exceptions/Exception.php
@@ -0,0 +1,9 @@
+<?php namespace Agency\Office\Auth\Authorization\Exceptions;
+
+/**
+ * @author Abed Halawi <abed.halawi@vinelab.com>
+ */
+
+use RuntimeException;
+
+class Exception extends RuntimeException {}
