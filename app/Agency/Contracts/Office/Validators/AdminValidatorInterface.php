037e873e78d2868b5b7fde205bd3cdab40ca5769
ibrahim fleifle <ibrahimfleifle@flay.local>
2014-11-20 20:37:25 +0100

    create AdminValidator Interface

---
+++ app/Agency/Contracts/Office/Validators/AdminValidatorInterface.php
@@ -0,0 +1,10 @@
+<?php namespace Agency\Contracts\Office\Validators;
+
+/**
+ * @author Abed Halawi <abed.halawi@vinelab.com>
+ */
+
+interface AdminValidatorInterface {
+
+    public function validateForUpdate($attributes);
+}
