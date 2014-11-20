6969a73ae16c3551e69eae172576ef02d7b090df
ibrahim fleifle <ibrahimfleifle@flay.local>
2014-11-20 20:43:40 +0100

    create Common Lang config file

---
+++ app/lang/common.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'title'             =>  'Sub Section Title',
+    'add'               =>  'Add',
+    'update'            =>  'Update',
+    'cancel'            =>  'Cancel'
+];
+
