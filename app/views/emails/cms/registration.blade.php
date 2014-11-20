22050301e0d57f9dd166a3e0508e4d4942057f13
ibrahim fleifle <ibrahimfleifle@flay.local>
2014-11-20 21:01:10 +0100

    change the view of the registration email

--- app/views/emails/cms/registration.blade.php
+++ app/views/emails/cms/registration.blade.php
@@ -1,5 +1,6 @@
 <body>
-    {{Lang::get('email.welcome_to_najem')}}!
+    <h1>{{Lang::get('email.welcome_to_agency')}}!</h1>
     <br /><br />
-    {{Lang::get('email.use_this_password_to_login')}}: {{ $password }}
+    {{Lang::get('email.use_this_password_to_login')}}: <br>
+    <h3>{{$password }}</h3>
 </body>
