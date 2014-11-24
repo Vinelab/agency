8079e54cd39132d62b80afa62ca3864fe2ff1f27
ibrahim fleifle <ibrahimfleifle@flay.local>
2014-11-24 10:14:48 +0100

    display contents sections

--- app/views/cms/pages/content/home.blade.php
+++ app/views/cms/pages/content/home.blade.php
@@ -1,12 +1,19 @@
+<?php
+    $content_section = Which::section();
+    $subsections = $content_section->children;
+?>
 @section("head")
     @parent
+    {{HTML::style(Cdn::asset('/css/content/posts.css'))}}
+
+
+
 
 @stop
 @section('content')
 
-
-	<div class="col-sm-6">
-		@if ($admin_permissions->has('create'))
+    <div>
+        @if (Auth::hasPermission('create'))
             <div class="row">
                 <a href="{{ URL::route('cms.content.posts.create') }}" class="btn btn-primary">
                     <span class="icon-plus"></span>
@@ -14,25 +21,27 @@
                 </a>
             </div>
         @endif
-        <ol class="dd-list">
-			@foreach($sections as $section)
-				<li class="dd-item" data-id="1">
-					<div class="dd-handle">
-						{{HTML::link(URL::route('cms.content.show',$section->alias),$section->title)}}
-					</div>
-				</li>
-			@endforeach
-			
+        <ul id='sub-section-list'>
+            @foreach($subsections as $section)
+                <li class='sub-section-items'>
+                    <a href="{{URL::route('cms.content.show',$section->alias)}}">
+                        <div class='btn btn-app btn-primary sections-button'>
+                            {{$section->title}}
+                        </div>
+                    </a>
+                </li>
+            @endforeach
 
-		</ol>
+
+        </ul>
     </div>
 @stop
 
 @section('scripts')
     @parent
-        {{HTML::script("/cms/js/content/index.js")}}
+        {{HTML::script(Cdn::asset('/cms/js/content/index.js'))}}
 
-        
+
 @stop
 
 @include('cms.layout.master')
