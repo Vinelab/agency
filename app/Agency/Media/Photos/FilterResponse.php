0dd6a4d0434b83da1e65beafcf9b191de91ce828
ibrahim fleifle <ibrahimfleifle@flay.local>
2014-11-27 18:38:58 +0100

    filter aws response
    copy this code from starac project

--- app/Agency/Media/Photos/FilterResponse.php
+++ app/Agency/Media/Photos/FilterResponse.php
@@ -6,46 +6,18 @@ use Agency\Contracts\HelperInterface;
 
 class FilterResponse implements FilterResponseInterface {
 
-    protected $helper;
-
-    public function __construct(HelperInterface $helper)
-    {
-        $this->helper = $helper;
-    }
-
     public function make($response)
     {
-        $without_original = [];
         $originals = [];
         foreach ($response as $image) {
-
-            $unique_id = $this->helper->getUniqueId();
-
-            array_push($without_original,[
-                'url' => $image['thumbnail']->url,
-                'preset' => 'thumbnail',
-                'guid' => $unique_id,
-            ]);
-
-            array_push($without_original,[
-                'url' => $image['small']->url,
-                'preset' => 'small',
-                'guid' => $unique_id,
-            ]);
-
-            array_push($without_original,[
-                'url' => $image['square']->url,
-                'preset' => 'square',
-                'guid' => $unique_id,
-            ]);
-
             array_push($originals, new Image([
-                'url' => $image['original']->url,
-                'preset' => 'original',
-                'guid' => $unique_id,
+                'original' => $image['original']->url,
+                'thumbnail'=> $image['thumbnail']->url,
+                'small' => $image['small']->url,
+                'square' => $image['square']->url
             ]));
         }
 
-        return compact('without_original','originals');
+        return ['originals' => $originals];
     }
 }
