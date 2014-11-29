77c79073aa9278f04338c86be5eae7d104044501
ibrahim fleifle <ibrahimfleifle@flay.local>
2014-11-29 19:05:29 +0100

    use CodeManagerInterface to delete the code from the cache after 2 days

--- app/controllers/Api/CodesController.php
+++ app/controllers/Api/CodesController.php
@@ -1,14 +1,17 @@
 <?php namespace Agency\Api\Controllers;
 
 
-use Agency\Api\Repositories\Contracts\ApplicationRepositoryInterface;
-use Agency\Api\Repositories\Contracts\CodeRepositoryInterface;
+use Agency\Contracts\Repositories\ApplicationRepositoryInterface;
+use Agency\Contracts\Repositories\CodeRepositoryInterface;
 use Agency\Api\Encryptors\EncryptorInterface;
 
 use Agency\Api\Validators\Contracts\EncryptorValidatorInterface;
 use Agency\Api\Validators\CodeValidator;
+use Agency\Contracts\Api\CodeManagerInterface;
 
-use Str,Response,Input, Controller;
+
+use Str,Response,Input, Controller, Config, Lang;
+use Carbon\Carbon;
 
 class CodesController extends Controller {
 
@@ -16,25 +19,29 @@ class CodesController extends Controller {
 								CodeValidator $validator,
 								ApplicationRepositoryInterface $application,
 								EncryptorInterface $encryptor,
-								EncryptorValidatorInterface $encryptorValidator)
+								EncryptorValidatorInterface $encryptorValidator,
+								CodeManagerInterface $manager)
 	{
 		$this->code  = $code;
 		$this->validator = $validator;
 		$this->application = $application;
 		$this->encryptor = $encryptor;
 		$this->encryptorValidator = $encryptorValidator;
+		$this->manager = $manager;
+
 	}
 
 	public function create()
-	{	
+	{
+
 		$key = Input::get('key');
 		try {
 			$application = $this->application->findBy("key",$key);
 			$secret = $application->secret;
 		} catch (Exception $e) {
-			return Response::json(["status"=>400,"messages"=>Lang::get("messages.invalid_app_credentials")]);
+			return Response::json(["status" => 400, "messages" => Lang::get("messages.invalid_app_credentials")]);
 		}
-		
+
 
 		if($this->encryptorValidator->validate(["key"=>$secret,"data"=>$key.$secret]))
 		{
@@ -52,15 +59,21 @@ class CodesController extends Controller {
 
 				if($this->validator->validate($input))
 				{
+
+					$duration = Carbon::now('Asia/Beirut')->addDays(2)->timestamp;
+
+					$duration_in_seconds = $duration - Carbon::now('Asia/Beirut')->timestamp;
+
+					$this->manager->store($code, $application->id, $duration_in_seconds);
 					$code = $this->code->create($application->id,$code,true);
-					return Response::json(['status'=>200,'code'=>$code->code]);
+					return Response::json(['status'=>200,'code'=>$code->code, 'duration'=> $duration]);
 				}
 
 			}
 		} else {
 			return Response::json(['status'=>400,'messages'=>Lang::get("messages.invalid_app_credentials")]);
 		}
-		
+
 	}
 
 }
