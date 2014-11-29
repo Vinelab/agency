e0023e2d430f1ea64d0f33268fc199338e1cae5b
ibrahim fleifle <ibrahimfleifle@flay.local>
2014-11-29 19:08:16 +0100

    fix typo in the edit post form

--- app/views/cms/pages/post/edit.blade.php
+++ app/views/cms/pages/post/edit.blade.php
@@ -3,14 +3,14 @@
 		@parent
 		{{HTML::style("/css/post/create.css")}}
 
-		<link rel="stylesheet" href="{{Cdn::(asset('css/jquery.Jcrop.min.css'))}}" />
+		<link rel="stylesheet" href="{{Cdn::asset('css/jquery.Jcrop.min.css')}}" />
 
-		<link rel="stylesheet" href="{{Cdn::(asset('assets/css/jquery-ui-1.10.3.full.min.css'))}}" >
-		<link rel="stylesheet" href="{{Cdn::(asset('css/bootstrap-datetimepicker.min.css'))}}">
-		<link rel="stylesheet" href="{{Cdn::(asset('css/jquery.Jcrop.min.css'))}}" />
-		<link rel="stylesheet" href="{{Cdn::(asset('assets/css/colorbox.css'))}}" />
-		<link rel="stylesheet" href="{{Cdn::(asset('css/style.css'))}}">
-		<script src="{{Cdn::(asset('js/jquery_1_9_1.min.js'))}}"></script>
+		<link rel="stylesheet" href="{{Cdn::asset('assets/css/jquery-ui-1.10.3.full.min.css')}}" >
+		<link rel="stylesheet" href="{{Cdn::asset('css/bootstrap-datetimepicker.min.css')}}">
+		<link rel="stylesheet" href="{{Cdn::asset('css/jquery.Jcrop.min.css')}}" />
+		<link rel="stylesheet" href="{{Cdn::asset('assets/css/colorbox.css')}}" />
+		<link rel="stylesheet" href="{{Cdn::asset('css/style.css')}}">
+		<script src="{{Cdn::asset('js/jquery_1_9_1.min.js')}}"></script>
 
 	@stop
 
