1e7b71ed1ff300ab50079e561ef82f091dba73bd
ibrahim fleifle <ibrahimfleifle@flay.local>
2014-11-29 18:54:04 +0100

    create ApplicationRepository and its Interface

---
+++ app/Agency/Repositories/ApplicationRepository.php
@@ -0,0 +1,36 @@
+<?php namespace Agency\Repositories;
+
+use Agency\Contracts\Repositories\ApplicationRepositoryInterface;
+use Agency\Contracts\ApplicationInterface;
+use Agency\Cms\Repositories\Repository;
+
+class ApplicationRepository extends Repository implements ApplicationRepositoryInterface {
+
+	/**
+	 * The Application instance
+	 * @var Application
+	 */
+	protected $application;
+
+	public function __construct(ApplicationInterface $application)
+	{
+		$this->model = $this->application = $application;
+	}
+
+	/**
+	 * Create new Application
+	 * @param  string $key
+	 * @param  string $secret
+	 * @return Application
+	 */
+	public function create($name,$key, $secret)
+	{
+		return $this->application->create(["name" => $name, "key" => $key, "secret" => $secret]);
+	}
+
+	public function destroy($id)
+	{
+		return $this->remove($id);
+	}
+
+}
