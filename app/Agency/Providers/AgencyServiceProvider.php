1185bfc19e4b4edbfb56bd07ac670ba3eeabea51
ibrahim fleifle <ibrahimfleifle@flay.local>
2014-12-01 16:45:54 +0100

    bind classes to their interfaces

--- app/Agency/Providers/AgencyServiceProvider.php
+++ app/Agency/Providers/AgencyServiceProvider.php
@@ -14,7 +14,7 @@ class AgencyServiceProvider extends ServiceProvider {
         // $this->app->register('Agency\Providers\ApiServiceProvider');
 
         // register Media Service Provider
-        // $this->app->register('Agency\Providers\MediaServiceProvider');
+        $this->app->register('Agency\Media\MediaServiceProvider');
 
         // register Cms Service Provider
         $this->app->register('Agency\Providers\OfficeServiceProvider');
@@ -22,6 +22,60 @@ class AgencyServiceProvider extends ServiceProvider {
         $this->app->bind(
             'Agency\Contracts\Repositories\TagRepositoryInterface',
             'Agency\Repositories\TagRepository');
+
+         // posts
+        $this->app->bind(
+            'Agency\Contracts\Repositories\PostRepositoryInterface',
+            'Agency\Repositories\PostRepository');
+
+
+        $this->app->bind(
+            'Agency\Contracts\Repositories\ApplicationRepositoryInterface',
+            'Agency\Repositories\ApplicationRepository');
+
+        $this->app->bind(
+            'Agency\Contracts\ApplicationInterface', 
+            'Agency\Api\Application');
+
+        $this->app->bind(
+            'Agency\Contracts\Repositories\CodeRepositoryInterface',
+            'Agency\Repositories\CodeRepository');
+
+        $this->app->bind(
+            'Agency\Contracts\CodeInterface', 
+            'Agency\Api\Code');
+
+        $this->app->bind(
+            'Agency\Api\Encryptors\EncryptorInterface',
+            'Agency\Api\Encryptors\Encryptor');
+
+        $this->app->bind(
+            'Agency\Api\Validators\Contracts\EncryptorValidatorInterface',
+            'Agency\Api\Validators\EncryptorValidator');
+
+        $this->app->bind(
+            'Agency\Contracts\Api\CodeManagerInterface',
+            'Agency\Api\CodeManager');
+
+        $this->app->bind(
+            'Agency\Contracts\Api\Validators\CodeValidatorInterface',
+            'Agency\Api\Validators\CodeValidator');
+
+        $this->app->bind(
+            'Agency\Contracts\Cache\CacheManagerInterface',
+            'Agency\Cache\CacheManager');
+      $this->app->bind(
+                'Agency\Contracts\Api\ApiInterface',
+                'Agency\Api\Api');
+
+
+
+
+
+
+
+
+
 
 
         // register API V1 Mappers Service Provider
@@ -74,5 +128,20 @@ class AgencyServiceProvider extends ServiceProvider {
         //     'Agency\Post\Repositories\StatusRepository');
 
         $this->app->bind('Agency\Contracts\HelperInterface','Agency\Support\Helper');
+
+
+
+        $this->app->bind(
+            'Agency\Contracts\Validators\PostValidatorInterface', function() {
+                return new \Agency\Validators\PostValidator($this->app->make('validator'));
+            });
+
+        $this->app->bind(
+            'Agency\Contracts\Validators\VideoValidatorInterface', function() {
+                return new \Agency\Validators\VideoValidator($this->app->make('validator'));
+            });
+
+
+
     }
 }
