4f3c9d544da2c14d6c1941b59f9ec64829d56b11
ibrahim fleifle <ibrahimfleifle@flay.local>
2014-12-02 10:58:41 +0100

    create Admin Intetrface

---
+++ app/Agency/Contracts/Office/AdminInterface.php
@@ -0,0 +1,12 @@
+<?php namespace Agency\Contracts\Office;
+
+interface AdminInterface {
+
+    /**
+     * return the admin's name
+     *
+     * @return string
+     */
+    public function getName();
+
+}
