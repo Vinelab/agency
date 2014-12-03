5e264e24854546d03cf3be858f5338d70916e5b5
ibrahim fleifle <ibrahimfleifle@flay.local>
2014-12-03 11:52:52 +0100

    display Trash Icon to delete image

--- app/views/cms/pages/templates/post.php
+++ app/views/cms/pages/templates/post.php
@@ -1,7 +1,7 @@
 <script type="text/x-handlebars-template" id="image_item" />
 	<li id="croped-image-item-{{index}}" class="croped-image-item">
 		<span  onclick="deleteImage('{{data}}',this)">
-			<i class="icon-trash bigger-130"></i>
+			<i class="ace-icon fa fa-trash-o bigger-120"></i>
 		</span>
 		<img class="image_to_crop" src="{{data}}" id="croped-img-{{index}}">
 	</li>
