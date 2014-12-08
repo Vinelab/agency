a4c2adcfaf8b9c56417d194ec58fc85b68a15e5b
ibrahim fleifle <ibrahimfleifle@flay.local>
2014-12-08 12:44:47 +0100

    add publisher facade to the service provider

--- app/Agency/Providers/CmsServiceProvider.php
+++ app/Agency/Providers/CmsServiceProvider.php
@@ -65,5 +65,6 @@ class CmsServiceProvider extends ServiceProvider {
                 'Agency\Cms\Auth\Authorization\Entities\Privilege');
 
         $this->app->singleton('which', 'Agency\Support\Which\Which');
+        $this->app->singleton('publisher', 'Agency\Support\Publisher\Publisher');
     }
 }
