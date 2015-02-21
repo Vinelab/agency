35a2e1f44c6a14434b4c582031227a5c3529f972
ibrahim fleifle <ibrahimfleifle@flay.local>
2015-02-21 18:13:19 +0100

    create score cache manager

---
+++ app/Xfactor/Cache/ScoreCacheManager.php
@@ -0,0 +1,13 @@
+<?php namespace Xfactor\Cache;
+
+/**
+ * @author Ibrahim Fleifel <ibrahim@vinelab.com>
+ */
+
+use Agency\Cache\CacheManager;
+
+class ScoreCacheManager extends CacheManager {
+
+    protected $prefix = 'score';
+
+}
