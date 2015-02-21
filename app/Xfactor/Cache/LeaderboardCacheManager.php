a88070b9985764ed575683a1cf1fc3520f5f9c98
ibrahim fleifle <ibrahimfleifle@flay.local>
2015-02-21 18:15:00 +0100

    create leaderboard cache manager

---
+++ app/Xfactor/Cache/LeaderboardCacheManager.php
@@ -0,0 +1,13 @@
+<?php namespace Xfactor\Cache;
+
+/**
+ * @author Ibrahim Fleifel <ibrahim@vinelab.com>
+ */
+
+use Agency\Cache\CacheManager;
+
+class LeaderboardCacheManager extends CacheManager {
+
+    protected $prefix = 'leaderboard';
+
+}
