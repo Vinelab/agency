90215276e7b458eab45a4e716666876fe65f12d4
ibrahim fleifle <ibrahimfleifle@flay.local>
2015-02-21 18:19:16 +0100

    add common lang file

--- app/lang/en/common.php
+++ app/lang/en/common.php
@@ -4,6 +4,7 @@ return [
     'title'             =>  'Sub Section Title',
     'add'               =>  'Add',
     'update'            =>  'Update',
-    'cancel'            =>  'Cancel'
+    'cancel'            =>  'Cancel',
+    'create'			=>  'Create'
 ];
 
