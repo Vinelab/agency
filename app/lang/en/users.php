348c4925f110f3ff4d6afe5e093ba9c24bd723d9
ibrahim fleifle <ibrahimfleifle@flay.local>
2015-02-21 18:14:26 +0100

    create user lang file

---
+++ app/lang/en/users.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'labels' => [
+        'name'              => 'Name',
+        'gigya_id'          => 'Gigya Id',
+        'wallet_votes'      => 'Votes',
+        'blocked'           => 'blocked',
+        'avatar'            => 'avatar',
+        'total'             => 'total'
+    ],
+
+    'messages' => [
+        'no_users' => "There's no users":
+    ]
+];
