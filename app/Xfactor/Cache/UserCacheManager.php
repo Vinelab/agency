9c12e0db34cf6cee2e115c75d937614d9cf17a75
ibrahim fleifle <ibrahimfleifle@flay.local>
2015-02-21 18:03:18 +0100

    create user cache

---
+++ app/Xfactor/Cache/UserCacheManager.php
@@ -0,0 +1,19 @@
+<?php namespace Xfactor\Cache;
+
+/**
+ * @author Ibrahim Fleifel <ibrahim@vinelab.com>
+ */
+
+use Agency\Cache\CacheManager;
+
+class UserCacheManger extends CacheManager {
+
+    protected $prefix = 'user';
+
+
+    public function createUserScore($id)
+    {
+    	
+    }
+
+}
