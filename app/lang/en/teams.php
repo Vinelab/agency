1906bfaf08b061f8a919548b4c9dd2b9f1fd1e46
ibrahim fleifle <ibrahimfleifle@flay.local>
2015-02-21 18:10:14 +0100

    add team lang file

---
+++ app/lang/en/teams.php
@@ -0,0 +1,21 @@
+<?php
+return [
+	'buttons' => [
+		'create' => 'Create',
+		'update' => 'Update',
+		'delete' => 'Delete'
+	],
+
+	'labels' => [
+		'photo' => 'Photo',
+		'title' => 'Title',
+		'no_teams' => "There's no teams please create one!",
+		'edit' => 'Edit'
+	],
+
+	'messages' => [
+		'created' => 'Team has been created successfully',
+		'deleted' => 'Team has been deleted successfully',
+		'updated' => 'Team has been updated successfully'
+	]
+];
