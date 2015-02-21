de1430e4c4ffd812f68fadb045101def758b06e8
ibrahim fleifle <ibrahimfleifle@flay.local>
2015-02-21 18:07:15 +0100

    create team cache

---
+++ app/Xfactor/Cache/TeamCacheManager.php
@@ -0,0 +1,13 @@
+<?php namespace Xfactor\Cache;
+
+/**
+ * @author Ibrahim Fleifel <ibrahim@vinelab.com>
+ */
+
+use Agency\Cache\CacheManager;
+
+class TeamCacheManager extends CacheManager {
+
+    protected $prefix = 'team';
+
+}
