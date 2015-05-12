cdb03bf608b6c5190bb971c813784b2943e41f49
bob flay <bob.fleifel@gmail.com>
2015-05-12 12:14:49 +0200

    move lang folder

---
+++ resources/lang/ar/email.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+	'reset_password_body' => 'لإعادة تعيين كلمة السرالرجاء الذهاب الى هذا الرابط',
+	'click_here' => 'إكبس هنا',
+	'welcome' => 'مرحبا',
+	'welcome_to_cms' => 'شرفتنا والله في مكان المحتوى لأبلة فهيتا',
+    'visit_this_link' => 'روح هنا علشان تخش',
+	'use_this_password_to_login' => "إستعمل كلمة السر ده للدخول",
+
+    'email_verification' => 'Abla Fahita - Verify Your Email',
+
+    'name' => 'الإسم',
+    'email' => 'البريد الإلكتروني',
+    'phone' => 'رقم الهاتف',
+    'country' => 'البلد',
+    'reasons' => 'الأسباب الخمسة',
+];
